e8fc6df7328fc286e4acebb6e949d53d6cd19e76
Tilda Dahlgren <tildahlgren@gmail.com>
2016-11-17 15:19:24 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+$('img').bind('contextmenu', function(e) {
+    return false;
+});
+
+
+$(document).ready(function() {
+	$('a[rel="relativeanchor"]').click(function(){
+	    $('html, body').animate({
+	        scrollTop: $( $.attr(this, 'href') ).offset().top
+	    }, 900);
+	    return false;
+	});
+});
